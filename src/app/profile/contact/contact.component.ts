91405b8ec50ca9e8dbc34f31eb107909495bcaf0
Dhruvil Patel <dhruvil.patel@pointofrental.com>
2021-02-17 16:35:01 -0600

    sent email to me

--- src/app/profile/contact/contact.component.ts
+++ src/app/profile/contact/contact.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../profile.service';
 import { SnotifyService } from 'ng-snotify';
 import { environment } from '../../../environments/environment';
-
+import { NgForm } from '@angular/forms';
+import { HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -12,33 +14,25 @@ export class ContactComponent implements OnInit {
  
   model: any = {};
 
-  constructor(){}
+  constructor(private http: HttpClient){}
 
   ngOnInit() {
      }
-
-
-  // snotifyConfig = environment.snotifyConfig;
-  // model: any = {};
-
-  // constructor(
-  //   private profile: ProfileService,
-  //   private snotify: SnotifyService
-  // ) { }
-
-  // ngOnInit() {
-  // }
-
-  // contact() {
-  //   this.profile.contactus(this.model).subscribe(data => {
-  //     if (data.status) {
-  //       this.snotify.success(data.message, 'Success', this.snotifyConfig);
-  //     } else {
-  //       this.snotify.warning(data.message, 'Warning', this.snotifyConfig);
-  //     }
-  //   }, err => {
-  //     this.snotify.error('Something went wrong. Try again later.', 'Error', this.snotifyConfig);
-  //   });
-  // }
+onSubmit(contactForm: NgForm) {
+    if (contactForm.valid) {
+      const email = contactForm.value;
+      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+      console.log("headers :", headers);
+      console.log("email name email message :", email, email.name, email.email, email.message)
+      this.http.post('https://formspree.io/f/mqkgodqj',
+        { name: email.name, replyto: email.email, message: email.messages },
+        { 'headers': headers }).subscribe(
+          response => {
+            alert("Your Message send to Dhruvil Patel, he will contact ASAP.")
+            // console.log(response);
+          }
+        );
+    }
+  }
 
 }
