6f1d7ebc3728363f2b65d079c1ba7e35b5ffe153
Dhruvil Patel <pateldhruvil01@yahoo.com>
2022-04-03 19:23:15 -0400

    added gh pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ const config: ExtraOptions = {
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes, config),
+    RouterModule.forRoot(routes),
     ProfileModule,
     HttpClientModule,
     BrowserAnimationsModule
